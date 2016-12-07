049ef086476bc45d539eea5f94103bcfd9348da4
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2016-12-07 23:54:27 +0100

    Updated new assets include paths.

--- functions.php
+++ functions.php
@@ -102,11 +102,9 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_template_directory_uri() . '/css/style.min.css' );
+	wp_enqueue_style( '_s-style', get_template_directory_uri() . '/css/style.css' );
 
-	wp_enqueue_script( '_s-app', get_template_directory_uri() . '/js/app.min.js', array( 'jquery' ), '', true );
-
-	wp_enqueue_script( '_s-main', get_template_directory_uri() . '/js/main.js', array(), '', true );
+	wp_enqueue_script( '_s-app', get_template_directory_uri() . '/js/app.js', array(), '', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
