1739d55dab7d573f3bd1b277eddd85bfe5ac07ba
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2016-12-09 22:37:49 +0100

    Added karma support for webpack.

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Fri Dec 09 2016 04:51:50 GMT+0100 (Central European Standard Time)
 
+var webpackConfig = require( './webpack.config.js' );
+
 module.exports = function( config ) {
 	config.set( {
 
@@ -13,7 +15,7 @@ module.exports = function( config ) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'js/app.js',
+			'js/main.js',
 			'tests/js/**/*.js'
 		],
 
@@ -24,7 +26,8 @@ module.exports = function( config ) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'tests/js/**/*.js': ['babel']
+			'js/main.js': ['webpack'],
+			'tests/js/**/*.js': ['webpack']
 		},
 
 		// test results reporter to use
@@ -44,7 +47,7 @@ module.exports = function( config ) {
 		logLevel: config.LOG_INFO,
 
 		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
+		autoWatch: false,
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -56,6 +59,14 @@ module.exports = function( config ) {
 
 		// Concurrency level
 		// how many browser should be started simultaneous
-		concurrency: Infinity
+		concurrency: Infinity,
+
+		// webpack configuration
+		webpack: webpackConfig,
+
+		// webpack-dev-middleware configuration
+		webpackMiddleware: {
+			stats: 'errors-only'
+		}
 	} );
 };
