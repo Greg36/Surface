87e30771d5e12332ebd7e21b5ac83d80b02b74d6
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2017-01-14 02:49:40 +0100

    More silienced output for gitlab ci config.

--- assets/build/ci_install.sh
+++ assets/build/ci_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install curl and wget
 apt-get update -yqq
-apt-get install curl -yqq
+apt-get install curl -yqq > /dev/null 2>&1
 apt-get install wget -yqq
 
 # Install php curl
@@ -49,8 +49,8 @@ apt-get install -yqq xvfb
 
 # Install Firefox
 export FIREFOX_VERSION=50.0
-apt-get -qqy --no-install-recommends install firefox
-rm -rf /var/lib/apt/lists/* /var/cache/apt/*
+apt-get -qqy --no-install-recommends install firefox > /dev/null 2>&1
+rm -rf /var/lib/apt/lists/* /var/cache/apt/* > /dev/null 2>&1
 wget --no-verbose -O /tmp/firefox.tar.bz2 https://download-installer.cdn.mozilla.net/pub/firefox/releases/$FIREFOX_VERSION/linux-x86_64/en-US/firefox-$FIREFOX_VERSION.tar.bz2
 apt-get -y purge firefox
 rm -rf /opt/firefox
@@ -58,7 +58,7 @@ tar -C /opt -xjf /tmp/firefox.tar.bz2
 rm /tmp/firefox.tar.bz2
 mv /opt/firefox /opt/firefox-$FIREFOX_VERSION
 ln -fs /opt/firefox-$FIREFOX_VERSION/firefox /usr/bin/firefox
-apt-get update
+apt-get update -yqq
 
 #apt-get install dbus
 #dbus-uuidgen > /var/lib/dbus/machine-id
@@ -66,18 +66,18 @@ apt-get update
 # Get Selenium server
 mkdir ~/selenium
 cd ~/selenium
-wget http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
+wget http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar > /dev/null 2>&1
 
 # Install Java8
 apt-get install software-properties-common -yqq
-add-apt-repository ppa:webupd8team/java -y
-apt-get update
+add-apt-repository ppa:webupd8team/java -yqq
+apt-get update -yqq
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
-apt-get install oracle-java8-installer -yqq
+apt-get install oracle-java8-installer -yqq > /dev/null 2>&1
 
 # Get Geckodriver for selenium
-wget https://github.com/mozilla/geckodriver/releases/download/v0.13.0/geckodriver-v0.13.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.13.0/geckodriver-v0.13.0-linux64.tar.gz > /dev/null 2>&1
 tar -xvzf geckodriver-v0.13.0-linux64.tar.gz
 rm geckodriver-v0.13.0-linux64.tar.gz
 chmod +x geckodriver
