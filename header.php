4e0bafa7700def1776a69ab887f2a717e660e69e
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2017-06-14 00:46:04 +0200

    Updated from upstream.

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
  * @package _s
  */
 
-?><!DOCTYPE html>
+?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -42,14 +42,13 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+			?>
 		</nav><!-- #site-navigation -->
-
-		<?php if ( get_header_image() ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-				<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-			</a>
-		<?php endif; // End header image check. ?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
