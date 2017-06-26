121c06c43bb4a1573a1da032855f8a8527b9a641
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2017-06-26 23:20:51 +0200

    Updated from upstream.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Additional features to allow styling of the templates
+ * Functions which enhance the theme by hooking into WordPress
  *
  * @package _s
  */
@@ -12,11 +12,6 @@
  * @return array
  */
 function _s_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
-
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
@@ -25,3 +20,13 @@ function _s_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', '_s_body_classes' );
+
+/**
+ * Add a pingback url auto-discovery header for singularly identifiable articles.
+ */
+function _s_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
+	}
+}
+add_action( 'wp_head', '_s_pingback_header' );
