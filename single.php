063321bbb698609146cad7cad39de6a7c5a3b75a
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2017-09-16 22:50:20 +0200

    Updated from upstream.

--- single.php
+++ single.php
@@ -15,9 +15,12 @@ get_header(); ?>
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			the_post_navigation( array(
+				'prev_text' => '<span class="nav-subtitle">' . __( 'Previous:', '_s' ) . '</span> <span class="nav-title">%title</span>',
+				'next_text' => '<span class="nav-subtitle">' . __( 'Next:', '_s' ) . '</span> <span class="nav-title">%title</span>',
+			) );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
