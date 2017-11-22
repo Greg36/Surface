3174ab737cc7bf59c97eb5e8df68c2de17413f06
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2017-11-22 23:52:27 +0100

    Changed JS unit test sample to just an example.

---
+++ tests/js/sample_test.js
@@ -0,0 +1,13 @@
+/*
+ * Test navigation accessibility aids
+ */
+describe('SampleTestSuit', function() {
+
+	describe('SimpleTest', function() {
+
+		it('should be true', function() {
+			let foo = true;
+			expect(foo).to.equal(true);
+		});
+	});
+});
