5b02c76b39056f0fd6ac8f15639d8b1ba3994d72
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2020-04-11 11:54:22 +0200

    Switched build tools from webpack 3 to webpack 4 using Laravel Mix.
    * Added Laravel Mix config for build and export based on justintadlock/mythic
    * ESlint now uses WordPress's rules
    * Stylelint is now included
    * Switched "build" folder name to "dist"

---
+++ webpack.mix.export.js
@@ -0,0 +1,83 @@
+/**
+ * Theme Export Script
+ *
+ * Exports the production-ready theme with only the files and folders needed for
+ * uploading to a site or zipping. Edit the `files` or `folders` variables if
+ * you need to change something.
+ *
+ * @package   Mythic
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright 2018 Justin Tadlock
+ * @link      https://themehybrid.com/themes/mythic
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
+ */
+
+// Import required packages.
+const mix     = require( 'laravel-mix' );
+const rimraf  = require( 'rimraf' );
+const fs      = require( 'fs' );
+
+// Folder name to export the files to.
+let exportPath = '_s';
+
+// Theme root-level files to include.
+let files = [
+	'style.css',
+	'changelog.md',
+	'functions.php',
+	'index.php',
+	'LICENSE',
+	'README.md',
+//	'readme.txt',        // Required for WordPress.org theme review.
+	'screenshot.png'
+];
+
+// Folders to include.
+let folders = [
+	'app',
+	'dist',
+	'resources/lang',
+	'inc',
+	'template-parts',
+//	'resources/js',      // Required for WordPress.org theme review.
+//	'resources/scss',    // Required for WordPress.org theme review.
+	'vendor'
+];
+
+// Delete the previous export to start clean.
+rimraf.sync( exportPath );
+
+// Copy PHP files from root directory
+mix.copy( './*.php', exportPath );
+
+// Loop through the root files and copy them over.
+files.forEach( file => {
+
+	if ( fs.existsSync( file ) ) {
+		mix.copy( file, `${exportPath}/${file}` );
+	}
+} );
+
+// Loop through the folders and copy them over.
+folders.forEach( folder => {
+
+	if ( fs.existsSync( folder ) ) {
+		mix.copyDirectory( folder, `${exportPath}/${folder}` );
+	}
+} );
+
+// Delete the `vendor/bin` and `vendor/composer/installers` folder, which can
+// get left over, even in production. Mix will also create an additional
+// `mix-manifest.json` file in the root, which we don't need.
+mix.then( () => {
+
+	let files = [
+		'mix-manifest.json',
+		`${exportPath}/vendor/bin`,
+		`${exportPath}/vendor/composer/installers`
+	];
+
+	files.forEach( file => {
+		rimraf.sync( file );
+	} );
+} );
