eac4d733af2dde652c2647cda6243e76dc087d3e
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2021-12-17 21:06:55 +0100

    Update build, watch and lint scripts.

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,7 @@
  * complexity of building out a Webpack configuration file. Use this file to
  * configure how your assets are handled in the build process.
  *
- * @link https://laravel.com/docs/5.6/mix
+ * @link https://laravel.com/docs/8.x/mix
  *
  * @package   justintadlock/mythic
  * @link      https://themehybrid.com/themes/mythic
@@ -13,10 +13,9 @@
  */
 
 // Import required packages.
-const mix               = require( 'laravel-mix' ),
-	  ImageminPlugin    = require( 'imagemin-webpack-plugin' ).default,
-	  CopyWebpackPlugin = require( 'copy-webpack-plugin' ),
-	  imageminMozjpeg   = require( 'imagemin-mozjpeg' );
+const mix                    = require( 'laravel-mix' ),
+	  CopyWebpackPlugin      = require( 'copy-webpack-plugin' ),
+	  ImageMinimizerPlugin   = require( 'image-minimizer-webpack-plugin' );
 
 /*
  * -----------------------------------------------------------------------------
@@ -48,7 +47,7 @@ if ( process.env.export ) {
  * folder in the theme.
  */
 const devPath  = 'assets';
-const distPath = 'dist';
+const distPath = '.';
 
 /*
  * Sets the path to the generated assets. By default, this is the `/dist` folder
@@ -62,8 +61,8 @@ mix.disableSuccessNotifications();
 /*
  * Set Laravel Mix options.
  *
- * @link https://laravel.com/docs/5.6/mix#postcss
- * @link https://laravel.com/docs/5.6/mix#url-processing
+ * @link https://laravel.com/docs/8.x/mix#postcss
+ * @link https://laravel.com/docs/8.x/mix#url-processing
  */
 mix.options( {
 	postCss        : [ require( 'postcss-preset-env' )() ],
@@ -73,33 +72,32 @@ mix.options( {
 /*
  * Builds sources maps for assets.
  *
- * @link https://laravel.com/docs/5.6/mix#css-source-maps
+ * @link https://laravel.com/docs/8.x/mix#css-source-maps
  */
 mix.sourceMaps();
 
 /*
  * Versioning and cache busting. Append a unique hash for production assets.
  *
- * @link https://laravel.com/docs/5.6/mix#versioning-and-cache-busting
+ * @link https://laravel.com/docs/8.x/mix#versioning-and-cache-busting
  */
-if (mix.inProduction()) {
-	mix.version();
-}
+mix.version();
+
 
 /*
  * Compile JavaScript.
  *
- * @link https://laravel.com/docs/5.6/mix#working-with-scripts
+ * @link https://laravel.com/docs/8.x/mix#working-with-scripts
  */
-mix.js( `${devPath}/js/main.js`,              'js' )
+mix.js( `${devPath}/js/main.js`,             'js' )
    .js( `${devPath}/js/admin/customizer.js`, 'js' );
 
 /*
  * Compile CSS. Mix supports Sass, Less, Stylus, and plain CSS, and has functions
  * for each of them.
  *
- * @link https://laravel.com/docs/5.6/mix#working-with-stylesheets
- * @link https://laravel.com/docs/5.6/mix#sass
+ * @link https://laravel.com/docs/8.x/mix#working-with-stylesheets
+ * @link https://laravel.com/docs/8.x/mix#sass
  * @link https://github.com/sass/node-sass#options
  */
 
@@ -111,7 +109,7 @@ var sassConfig = {
 };
 
 // Compile SASS/CSS.
-mix.sass( `${devPath}/css/style.scss`, 'css', sassConfig );
+mix.sass( `${devPath}/css/style.scss`, 'css' ).options( sassConfig );
 
 /*
  * Add custom Webpack configuration.
@@ -120,7 +118,7 @@ mix.sass( `${devPath}/css/style.scss`, 'css', sassConfig );
  * function, so we're using the `CopyWebpackPlugin` for processing and copying
  * images into the distribution folder.
  *
- * @link https://laravel.com/docs/5.6/mix#custom-webpack-configuration
+ * @link https://laravel.com/docs/8.x/mix#custom-webpack-configuration
  * @link https://webpack.js.org/configuration/
  */
 mix.webpackConfig( {
@@ -129,45 +127,57 @@ mix.webpackConfig( {
 	performance : { hints  : false },
 	plugins     : [
 		// @link https://github.com/webpack-contrib/copy-webpack-plugin
-		new CopyWebpackPlugin( [
-			{ from : `${devPath}/img`,   to : `${distPath}/img`   },
-			{ from : `${devPath}/svg`,   to : `${distPath}/svg`   },
-			{ from : `${devPath}/fonts`, to : `${distPath}/fonts` }
-		] ),
-		// @link https://github.com/Klathmon/imagemin-webpack-plugin
-		new ImageminPlugin( {
-			test     : /\.(jpe?g|png|gif|svg)$/i,
-			disable  : process.env.NODE_ENV !== 'production',
-			optipng  : { optimizationLevel : 3 },
-			gifsicle : { optimizationLevel : 3 },
-			pngquant : {
-				quality : '65-90',
-				speed   : 4
-			},
-			svgo : {
-				plugins : [
-					{ cleanupIDs                : false },
-					{ removeViewBox             : false },
-					{ removeUnknownsAndDefaults : false }
+		new CopyWebpackPlugin(
+			{
+				patterns: [
+					{ from : `${devPath}/img`,   to : `${distPath}/img`   },
+					{ from : `${devPath}/svg`,   to : `${distPath}/svg`   },
+					{ from : `${devPath}/fonts`, to : `${distPath}/fonts` }
 				]
+			}
+		),
+		new ImageMinimizerPlugin({
+			minimizer: {
+				implementation: ImageMinimizerPlugin.imageminMinify,
+				options: {
+					// Lossy optimization with custom option
+					plugins: [
+						["gifsicle", { optimizationLevel : 3 }],
+						["mozjpeg", { quality: 75 }],
+						["pngquant", { quality: [0.65, 0.9], speed: 4 }],
+						// Svgo configuration here https://github.com/svg/svgo#configuration
+						[
+							"svgo",
+							{
+								plugins: {
+									name: 'preset-default',
+									overrides: {
+										removeViewBox: false,
+										cleanupIDs: false,
+										removeUnknownsAndDefaults: false,
+										addAttributesToSVGElement: {
+											attributes: [{ xmlns: "http://www.w3.org/2000/svg" }]
+										}
+									}
+								},
+							},
+						],
+					],
+				},
 			},
-			plugins : [
-				// @link https://github.com/imagemin/imagemin-mozjpeg
-				imageminMozjpeg( { quality : 75 } )
-			]
-		} )
+		})
 	]
 } );
 
-if ( process.env.sync ) {
+if ( process.argv.includes('--hot') ) {
 
 	/*
 	 * Monitor files for changes and inject your changes into the browser.
 	 *
-	 * @link https://laravel.com/docs/5.6/mix#browsersync-reloading
+	 * @link https://laravel.com/docs/8.x/mix#browsersync-reloading
 	 */
 	mix.browserSync( {
-		proxy : '__url__',
+		proxy : 'localhost/surface',
 		files : [
 			'dist/**/*',
 			'./**/*.php',
